01075a2a47727b4611f1fbec89d7403157df40e7
Ye Tun Aung <yta.megroup@gmail.com>
2019-07-15 14:33:24 +0630

    Checking Chapter 01-04

--- 03-Forms-and-Validation/flaskblog.py
+++ 03-Forms-and-Validation/flaskblog.py
@@ -55,10 +55,10 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+            flash(f'You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
